cdd5d23f70d4009cfe755fa4d08279435ad488ca
felipesanderp <felipe.panisson@hotmail.com>
2020-07-21 15:25:58 -0300

    add TodoList styles

--- src/pages/Todo/styles.ts
+++ src/pages/Todo/styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
+import { Form as Unform } from '@unform/web';
 
-export const Container = styled.div`
+export const Container = styled.div``;
+
+export const Form = styled(Unform)`
   max-width: 700px;
   width: 100%;
   height: 60px;
@@ -17,4 +20,37 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+`;
+
+export const Content = styled.div`
+  max-width: 700px;
+  height: 60px;
+  background: ${(props) => props.theme.colors.background};
+  margin: 60px auto;
+  border-style: dashed;
+  border-color: ${(props) => props.theme.colors.text};
+  border-radius: 5px 5px 5px 5px;
+  padding: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + div {
+    margin-top: -30px;
+  }
+`;
+
+export const TodoSyle = styled.div`
+  max-width: 750px;
+  height: 100%;
+  flex: 1;
+  display: flex;
+  align-items: center;
+
+  ul {
+    list-style: none;
+    color: ${(props) => props.theme.colors.text};
+    font-size: 20px;
+  }
 `;
